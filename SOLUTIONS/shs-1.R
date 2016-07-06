45e53b022b47a737994bd1a670b1b6dc21516563
Thomas Lumley <t.lumley@auckland.ac.nz>
2016-07-07 09:55:26 +1200

    more scottish internets

--- SOLUTIONS/shs-1.R
+++ SOLUTIONS/shs-1.R
@@ -10,3 +10,10 @@ f<-svysmooth(intuse~age,design=subset(dshs,sex=="female"))
 plot(rep(16:80,2),coef(byagesex),pch=c(1,19),ylim=c(0,1),xlab="Age",ylab="Proportion internet users")
 lines(m,lty=2)
 lines(f,lty=1)
+
+## with logistic regression
+agemodel<-svyglm(intuse~(pmin(age,30)+pmax(age,30)),design=dshs,family=quasibinomial)
+
+fittedage<-predict(agemodel,newdata=data.frame(age=16:80),type="response")
+lines(16:80,(fittedage),col="sienna")
+
